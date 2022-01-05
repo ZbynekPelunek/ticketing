343feec9e547da70d30552fd8a1f3db1c627b2a1
Zbynek Pelunek <34337408+ZbynekPelunek@users.noreply.github.com>
2022-01-05 18:13:02 +0100

    updated ports again

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -42,8 +42,8 @@ const start = async () => {
         console.error(err);
     }
 
-    app.listen(80, () => {
-        console.log('Listening on port 80!!!!');
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!!!');
     });
 };
 
